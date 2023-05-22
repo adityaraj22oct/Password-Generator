a16dfc3980a9cec9325dcfd7290031254a132a5d
adityaraj22oct <129570789+adityaraj22oct@users.noreply.github.com>
2023-05-22 20:48:52 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_password(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-="
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    return password
